ec1a1a3e9aae879da2b9effdbe54bc98d7437bb2
Seunghye.Stella.Kim <120572485+stella3760@users.noreply.github.com>
2023-02-22 17:37:53 +0000

    1.5 JAVA Study

---
+++ Chap_05/_Quiz_05.java
@@ -0,0 +1,16 @@
+package JAVA.Chap_05;
+
+public class _Quiz_05 {
+    public static void main(String[] args) {
+
+        int[] sizeArray = new int [10];
+        for (int i = 0; i < sizeArray.length; i++) {
+            sizeArray[i] = 250 + (5 * i);
+        }
+
+        for (int size : sizeArray) {
+            System.out.println("사이즈 " + size + " (재고있음)");
+        }
+
+    } // main
+} // class
